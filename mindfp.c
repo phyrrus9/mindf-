a9e0b2f6a6ada02d1e963f1ccd2651cf63b073d2
Ethan Laur <phyrrus9@gmail.com>
2013-01-17 21:56:56 -0500

    Version 5 added new commands and update help (see README)

--- mindfp.c
+++ mindfp.c
@@ -25,6 +25,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MPP_VERSION     5
+
 #define OP_END          0
 #define OP_INC_DP       1
 #define OP_DEC_DP       2
@@ -44,6 +46,8 @@
 #define OP_SPECIAL      16
 #define OP_GO_JMP       17
 #define OP_JMP_IF_END   18
+#define OP_FUNC_CALL    19
+#define OP_MEM_BLK      20
 
 #define SUCCESS         0
 #define FAILURE         1
@@ -63,8 +67,11 @@ struct instruction_t {
     unsigned short operand;
 };
 
+static char PROGRAM_SPAWN_CALL[55];
+
 static unsigned char compile = 0;
 static unsigned char execute = 0;
+static unsigned char memdump = 0;
 static unsigned short prog_end = 0;
 
 //define program containers (and stacks)
@@ -72,11 +79,15 @@ static struct instruction_t PROGRAM[PROGRAM_SIZE];
 static unsigned short STACK[STACK_SIZE];
 static unsigned int JUMPS[STACK_SIZE];
 static unsigned int IN_CONS[STACK_SIZE];
+static unsigned int FCALLS[STACK_SIZE];
 static unsigned int SP = 0;
 static unsigned int JP = 0;
 static unsigned int JC = 0;
 static unsigned int CP = 0;
 static unsigned int CC = 0;
+static unsigned int FC = 0;
+static unsigned int FP = 0;
+
 
 unsigned short data[DATA_SIZE];
 
@@ -89,14 +100,20 @@ static unsigned short LOOP_POS = 0;
 static int JMP_TEST_VAL = 0;
 static int GO_TO_X = 0;
 
-void stack_reverse(unsigned int *LIST, unsigned int LIST_SIZE) {
-    int i, j;
-    unsigned int *tmp = (unsigned int *)malloc(LIST_SIZE);
-    for (i = 0, j = LIST_SIZE; i < LIST_SIZE; i++, j--) {
-        tmp[i] = LIST[j];
+void call_function(int func_num)
+{
+    char func_file_name[17];
+    char func_call_comd[52];
+    FILE *fp;
+    int i;
+    sprintf(func_file_name, "%d.ms", func_num);
+    fp = fopen(func_file_name, "wb");
+    for (i = 0; i < PROGRAM_SIZE; i++) {
+        fprintf(fp, "%c", data[i]);
     }
-    free(LIST);
-    LIST = tmp;
+    fclose(fp);
+    sprintf(func_call_comd, "%s em %d.mp %s", PROGRAM_SPAWN_CALL, func_num, func_file_name);
+    system(func_call_comd);
 }
 
 int compile_bf(FILE* fp) {
@@ -111,6 +128,7 @@ int compile_bf(FILE* fp) {
             case '-': PROGRAM[pc].operator = OP_DEC_VAL; break;
             case '.': PROGRAM[pc].operator = OP_OUT; break;
             case ',': PROGRAM[pc].operator = OP_IN; break;
+            case 'B': PROGRAM[pc].operator = OP_MEM_BLK; break;
             case '=':
                 PROGRAM[pc].operator = OP_IN_CONS;
                 fscanf(fp, "%d", &tc);
@@ -194,6 +212,11 @@ int compile_bf(FILE* fp) {
             case 'H':
                 PROGRAM[pc].operator = OP_GO_JMP;
                 break;
+            case 'C':
+                fscanf(fp, "%d", &tc);
+                FCALLS[FC++] = tc;
+                PROGRAM[pc].operator = OP_FUNC_CALL;
+                break;
             default: pc--; break;
         }
         pc++;
@@ -209,7 +232,7 @@ int compile_bf(FILE* fp) {
 int execute_bf() {
     unsigned short pc = 0;
     unsigned int ptr = DATA_SIZE;
-    while (--ptr) { data[ptr] = 0; }
+    //while (--ptr) { data[ptr] = 0; }
     ptr = 0;
     while (PROGRAM[pc].operator != OP_END && ptr < DATA_SIZE) {
         switch (PROGRAM[pc].operator) {
@@ -226,6 +249,10 @@ int execute_bf() {
             case OP_SPECIAL:
             case OP_IN_CONS_C: data[ptr] = IN_CONS[CP++]; break;
             case OP_GO_JMP: ptr = 0; break;
+            case OP_MEM_BLK: memset(data, 0, DATA_SIZE); break;
+            case OP_FUNC_CALL:
+                call_function(FCALLS[FP++]);
+                break;
             case OP_SETJMP:
                 JMP_TEST_VAL = JUMPS[JP++];
                 break;
@@ -256,24 +283,48 @@ int main(int argc, const char * argv[])
 {
     int status, i;
     FILE *fp;
-    if (argc != 2 && argc != 3) {
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
-        return FAILURE;
+    char memdumpfname[17];
+    sprintf(PROGRAM_SPAWN_CALL, "%s", argv[0]);
+    memset(data, 0, DATA_SIZE);
+    memset(JUMPS, 0, STACK_SIZE);
+    if (argc == 2) {
+        fp = fopen(argv[1], "r");
     }
-    else if (argc == 3) {
+    else if (argc >= 3) {
         if (argv[1][0] == 'c') {
             compile = 1;
         }
         if (argv[1][0] == 'e') {
+            if (argv[1][1] == 'm' && argc > 3) {
+                memdump = atoi(argv[3]);
+                //printf("memory loaded: %d.ms\n", memdump);
+                sprintf(memdumpfname, "%d.ms", memdump);
+                fp = fopen(memdumpfname, "rb");
+                for (i = 0; i < DATA_SIZE; i++) {
+                    fscanf(fp, "%c", (char *)&data[i]);
+                }
+                fclose(fp);
+                //exit(1);
+            }
             execute = 1;
         }
         fp = fopen(argv[2], "r");
     }
     else {
-        fp = fopen(argv[1], "r");
+        fprintf(stderr, "Version: %d\n"
+                        "Usage: %s <opts> <file> <memdump>\n"
+                        "opts: optional field\n"
+                        "->c - compile\n"
+                        "->e - execute\n"
+                        "->em- execute function (requires memdump)\n"
+                        "file: required field\n"
+                        "->filename of program or function\n"
+                        "memdump: used only with em\n"
+                        "->specifies memory image to load\n"
+                        "-->NOT FOR CONSUMER USE\n",
+                        MPP_VERSION, argv[0]);
+        exit(-1);
     }
-    memset(data, 0, DATA_SIZE);
-    memset(JUMPS, 0, STACK_SIZE);
     if(execute == 0) {
         status = compile_bf(fp);
     }
@@ -295,6 +346,9 @@ int main(int argc, const char * argv[])
                     break;
                 case OP_SETJMP:
                     fprintf(fp, "%c", (char)JUMPS[JP++]);
+                    break;
+                case OP_FUNC_CALL:
+                    fprintf(fp, "%c", (char)FCALLS[FP++]);
                     break;
             }
         }
@@ -318,6 +372,9 @@ int main(int argc, const char * argv[])
                     break;
                 case OP_SETJMP:
                     fscanf(fp, "%c", (char *)&JUMPS[CC++]);
+                    break;
+                case OP_FUNC_CALL:
+                    fscanf(fp, "%c", (char *)&FCALLS[FC++]);
                     break;
             }
             prog_end = i;
